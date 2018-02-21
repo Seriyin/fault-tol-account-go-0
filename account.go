5251b878d218150c4ae19885beb9b35173e74abb
André Diogo <redan.blue27@gmail.com>
2018-02-21 01:13:21 +0000

    Fix account scoping and package with bankimpl.

--- account.go
+++ account.go
@@ -1,29 +1,27 @@
-package account
+package main
 
-type Interface interface{
+type Interface interface {
 	balance() uint64
 	movement(int64) bool
 }
 
 type Account struct {
-	balance int64
+	bal uint64
 }
 
-func (acc Account *) balance() uint64 {
-	return acc.balance
+func (acc *Account) balance() uint64 {
+	return acc.bal
 }
 
-func (acc Account *) movement(mov int64) bool {
+func (acc *Account) movement(mov int64) bool {
 	res := true
 	if mov > 0 {
-		acc.balance += mov
-	}
-	else {
-		if -mov > acc.balance {
+		acc.bal += uint64(mov)
+	} else {
+		if uint64(-mov) > acc.bal {
 			res = false
-		}
-		else {
-			acc.balance -= -mov
+		} else {
+			acc.bal -= uint64(-mov)
 		}
 	}
 	return res
