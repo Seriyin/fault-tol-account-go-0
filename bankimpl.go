d6e3f6706d9a507c7110530575b630f3250f53e4
André Diogo <redan.blue27@gmail.com>
2018-02-21 01:14:07 +0000

    Force TCP to stream files. Fix up assortment of bugs.

--- bankimpl.go
+++ bankimpl.go
@@ -1,49 +1,50 @@
 package main
 
 import (
-	"net"
 	"bufio"
-	"io"
 	"fmt"
+	"net"
 	"os"
-	"account"
-	"strings"
 	"strconv"
+	"strings"
 )
 
 type reply struct {
-	op byte
-	res bool
-	balance int64
+	op      byte
+	res     bool
+	balance uint64
 }
 
 type message struct {
-	op byte
+	op  byte
 	mov int64
 	rep chan reply
 }
 
-
 func main() {
 	// Listen on TCP port 2000 on all available unicast and
 	// anycast IP addresses of the local system.
-	l, err := net.Listen("tcp", "127.0.0.1:22556")
+	ip := net.IPv4(127, 0, 0, 1)
+	tcp := new(net.TCPAddr)
+	tcp.IP = ip
+	tcp.Port = 22556
+	l, err := net.ListenTCP("tcp", tcp)
 	if err != nil {
 		panic(err)
 	}
 	defer l.Close()
 	accchan := make(chan message, 100)
-	go func accounter(accchan)
+	go accounter(accchan)
 	for {
 		// Wait for a connection.
-		conn, err := l.Accept()
+		conn, err := l.AcceptTCP()
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Failed connect %s", err.String())
+			fmt.Fprintf(os.Stderr, "Failed connect %s", err)
 		}
 		f, err := conn.File()
 		conn.Close()
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Failed opening in blocking mode %s", err.String())
+			fmt.Fprintf(os.Stderr, "Failed opening in blocking mode %s", err)
 		}
 		// Handle the connection in a new goroutine.
 		// The loop then returns to accepting, so that
@@ -56,16 +57,16 @@ func main() {
 func accounter(accchan chan message) {
 	acc := new(Account)
 	for {
-		m := <- accchan
+		m := <-accchan
 		switch m.op {
-		case 0: 
-			m.rep <- reply{0,true,acc.balance()}
+		case 0:
+			m.rep <- reply{0, true, acc.balance()}
 		case 1:
-			m.rep <- reply{1,acc.movement(m.mov),0}
+			m.rep <- reply{1, acc.movement(m.mov), 0}
 		case 10:
 			return
 		default:
-			m.rep <- reply{255,false,0}
+			m.rep <- reply{255, false, 0}
 		}
 	}
 }
@@ -77,43 +78,42 @@ func handleConn(c *os.File, a chan message) {
 	out := bufio.NewWriter(c)
 	ch := make(chan reply, 50)
 	for in.Scan() {
-		if handleWords(a, in.Text()) {
+		if handleWords(ch, a, in.Text()) {
 			handleReply(ch, out)
 		}
 	}
-	if err := f.Err(); err != nil {
-		fmt.Fprintf("Error scanning %s", err.String())
+	if err := in.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error scanning %s", err)
 	}
 }
 
-func handleWords(a chan message, text string Text) (res bool) {
-	words := string.split(text," ")
+func handleWords(ch chan reply, a chan message, text string) (res bool) {
+	words := strings.Split(text, " ")
 	res = true
 	switch words[0] {
-	case "mov" :
-		mov, err := strconv.ParseInt(words[1])
+	case "mov":
+		mov, err := strconv.ParseInt(words[1], 10, 64)
 		if err != nil {
-			fmt.Fprintf("Error parsing mov int %s",err.String())
+			fmt.Fprintf(os.Stderr, "Error parsing mov int %s", err)
 			res = false
+		} else {
+			a <- message{1, mov, ch}
 		}
-		else {
-			a <- message{1,mov,ch}
-		}
-	case "bal" :
-		a <- message{0,0,ch}
+	case "bal":
+		a <- message{0, 0, ch}
 	default:
 		res = false
 	}
 	return
 }
 
-func handleReply(ch chan reply, out *Writer) {
-	rep := <- ch
+func handleReply(ch chan reply, out *bufio.Writer) {
+	rep := <-ch
 	switch rep.op {
 	case 0:
-		out.WriteString(fmt.Sprintf("bal %d",rep.balance))
+		out.WriteString(fmt.Sprintf("bal %d", rep.balance))
 	case 1:
-		out.WriteString(fmt.Sprintf("mov %t",rep.res))
+		out.WriteString(fmt.Sprintf("mov %t", rep.res))
 	case 255:
 		out.WriteString("unk")
 	}
