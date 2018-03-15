0e6f0c8d77d25c5524a821b7bc3be028a5fdbb67
André Diogo <redan.blue27@gmail.com>
2018-03-15 20:22:43 +0000

    Refactor client to delegate operations to bank interface.

--- client/testecli.go
+++ client/testecli.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"encoding/gob"
 	"fmt"
 	"math/rand"
-	"net"
-
-	"github.com/Seriyin/lab0-go/bank"
 )
 
 func main() {
@@ -15,24 +11,10 @@ func main() {
 	for i := range ch {
 		ch[i] = make(chan int64, 1)
 	}
-	ip := net.IPv4(127, 0, 0, 1)
-	tcp := new(net.TCPAddr)
-	tcp.IP = ip
-	tcp.Port = 22556
-	master, err := net.DialTCP("tcp", nil, tcp)
-	if err != nil {
-		panic(err)
-	}
-	defer master.Close()
-	dec := gob.NewDecoder(master)
-	enc := gob.NewEncoder(master)
+	bf := NewBankFactory()
+	b := bf.NewBank()
 	for i := 0; i < n; i++ {
-		f, err := net.DialTCP("tcp", nil, tcp)
-		if err != nil {
-			// handle error
-			panic(err)
-		}
-		go spamOps(f, ch[i])
+		go spamOps(bf, ch[i], i)
 	}
 	r := int64(0)
 	for _, c := range ch {
@@ -40,27 +22,24 @@ func main() {
 			r += i
 		}
 	}
-	rep := new(bank.Reply)
-	enc.Encode(bank.Message{Op: 0, Mov: 0})
-	dec.Decode(rep)
-	fmt.Printf("Got %d, Expected %d\n", r, rep.Balance)
+	fmt.Printf("Got %d, Expected %d\n", r, b.Balance())
+	bf.CloseBanks()
 }
 
-func spamOps(conn net.Conn, ch chan int64) {
-	defer conn.Close()
-	dec := gob.NewDecoder(conn)
-	enc := gob.NewEncoder(conn)
-	r := new(bank.Reply)
+func spamOps(bf *bankFactory, ch chan int64, index int) {
+	b := bf.NewBank()
 	sum := int64(0)
-	for i := rand.Intn(30000) + 50000; i > 0; i-- {
-		enc.Encode(bank.Message{Op: 1, Mov: rand.Int63n(400) - 200})
-		dec.Decode(r)
-		if r.Res {
-			sum += r.Balance
+	r := rand.Intn(30000) + 50000
+	fmt.Printf("%d Spammer will spam %d iterations", index, r)
+	for i := 0; i < r; i++ {
+		mov := rand.Int63n(400) - 200
+		if b.Movement(mov) {
+			sum += mov
 		} else {
-			fmt.Printf("Rejected %d\n", r.Balance)
+			fmt.Printf("Rejected %d\n", mov)
 		}
 	}
+	fmt.Printf("%d Spammer got %d", index, sum)
 	ch <- sum
 	close(ch)
 }
