12d6d43255c70f95174bf5aa62acc95e7370a8db
André Diogo <redan.blue27@gmail.com>
2018-06-26 14:03:16 +0100

    Makes a new random generator from a true random source.

--- client/testecli.go
+++ client/testecli.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	cr "crypto/rand"
 	"math/rand"
+	"encoding/binary"
 )
 
+var rnd *rand.Rand
+
 func main() {
-	n := rand.Intn(12) + 4
+	var seed int64
+	binary.Read(cr.Reader, binary.LittleEndian, &seed)
+	rnd = rand.New(rand.NewSource(seed))
+	n := rnd.Intn(8)+4
 	ch := make([]chan int64, n, n)
 	for i := range ch {
 		ch[i] = make(chan int64, 1)
@@ -29,14 +36,14 @@ func main() {
 func spamOps(bf *bankFactory, ch chan int64, index int) {
 	b := bf.NewBank()
 	sum := int64(0)
-	r := rand.Intn(30000) + 50000
+	r := rnd.Intn(30000) + 50000
 	fmt.Printf("%d Spammer will spam %d iterations\n", index, r)
 	for i := 0; i < r; i++ {
-		mov := rand.Int63n(400) - 200
+		mov := rnd.Int63n(400) - 200
 		if b.Movement(mov) {
 			sum += mov
 		} else {
-			fmt.Printf("Rejected %d\n", mov)
+			fmt.Printf("Rejected %d Movement of: %d\n", i, mov)
 		}
 	}
 	fmt.Printf("%d Spammer got %d\n", index, sum)
