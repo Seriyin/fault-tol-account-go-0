1a42ab06655300f86ab5392e12699e87f997ceea
André Diogo <redan.blue27@gmail.com>
2018-06-26 14:05:34 +0100

    Tightens movement logic + Abstracts account struct.

--- bank/account.go
+++ bank/account.go
@@ -1,9 +1,13 @@
-package main
+package bank
 
 //Account is a struct with an integer balance.
 //It implements the Bank interface.
 type account struct {
 	bal int64
+}
+
+func NewAccount() *account {
+	return &account{}
 }
 
 func (acc *account) Balance() int64 {
@@ -14,10 +18,10 @@ func (acc *account) Movement(mov int64) bool {
 	res := true
 	if mov > 0 {
 		acc.bal += mov
-	} else if -mov > acc.bal {
-		res = false
+	} else if -mov <= acc.bal {
+		acc.bal += mov
 	} else {
-		acc.bal -= -mov
+		res = false
 	}
 	return res
 }
